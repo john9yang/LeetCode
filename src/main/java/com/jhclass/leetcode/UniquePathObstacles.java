869ff1be67dff5c27f75215759722e5895b8ad11
John_Yang <jiahong9yang@126.com>
2019-05-11 10:08:38 +0800

    unique path with obstacle

---
+++ src/main/java/com/jhclass/leetcode/UniquePathObstacles.java
@@ -0,0 +1,38 @@
+package com.jhclass.leetcode;
+
+public class UniquePathObstacles {
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{{0,0,0},{0,1,0},{0,0,0}};
+        System.out.println(new UniquePathObstacles().uniquePathsObstacles(grid));
+    }
+
+    public int uniquePathsObstacles(int[][] obstacleGrid){
+        if ( obstacleGrid == null || obstacleGrid.length < 1 || obstacleGrid[0].length < 1
+             || obstacleGrid[0][0] == 1 || obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1]==1){
+            return 0;
+        }
+
+        int rows = obstacleGrid.length;
+        int cols = obstacleGrid[0].length;
+        int[][] result = new int[rows][cols];
+
+        //第一个位置有多少条路径，无障碍就是1条，有障碍就是0条
+        result[0][0] = obstacleGrid[0][0]== 0 ? 1:0;
+
+        for( int i = 1; i< cols;i++ ){
+            result[0][i] = obstacleGrid[0][i] == 0 ? result[0][i-1] : 0;
+        }
+
+        for( int j = 1; j< rows;j++ ){
+            result[j][0] = obstacleGrid[j][0] == 0 ? result[j-1][0] : 0;
+        }
+
+        for( int i=1;i<rows;i++ )
+            for( int j=1;j<cols;j++ ){
+                result[i][j] = obstacleGrid[i][j] == 0 ? result[i-1][j]+result[i][j-1] : 0;
+            }
+
+        return result[rows-1][cols-1];
+    }
+}
