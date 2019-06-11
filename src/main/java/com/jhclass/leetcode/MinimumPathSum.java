1d2e9fda2a80e76ec8750e9bcbef298ee7ddee12
John_Yang <jiahong9yang@126.com>
2019-06-11 16:12:50 +0800

    hamming distance

--- src/main/java/com/jhclass/leetcode/MinimumPathSum.java
+++ src/main/java/com/jhclass/leetcode/MinimumPathSum.java
@@ -4,12 +4,13 @@ public class MinimumPathSum {
 
     public static void main(String[] args) {
         MinimumPathSum minimumPathSum = new MinimumPathSum();
-        int[][] grid = new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};//7
-//        int[][] grid = new int[][]{{1, 2}, {3, 4}};//7
+//        int[][] grid = new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};//7
+        int[][] grid = new int[][]{{1, 2}, {3, 4}};//7
 //        int[][] grid = new int[][]{{1, 2}, {3, 4}, {5, 6}, {7, 8}};//21
 //        int[][] grid = new int[][]{{1}};//1
 //        int[][] grid = new int[][]{{1, 2, 5}, {3, 2, 1}};//7
-        System.out.println(minPath(grid,3,3));
+        //System.out.println(minPath(grid,3,3));
+        System.out.println(minPathSum2(grid));
     }
 
     public static int minPath(int[][] arr,int n,int m){
@@ -32,4 +33,48 @@ public class MinimumPathSum {
         return dp[n - 1][m - 1];
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // 动态归划和分枝限界，下面的方法会超时
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    public static int minPathSum2(int[][] grid) {
+        // 参数检验
+        if (grid == null || grid.length < 1 || grid[0].length < 1) {
+            return 0;
+        }
+
+        // 用于记录最小的路径各
+        int[] minSum = {Integer.MAX_VALUE};
+        int[] curSum = {0};
+        // 解题
+        solve(grid, 0, 0, curSum, minSum);
+
+        // 返回结果
+        return minSum[0];
+    }
+
+    public static void solve(int[][] grid, int row, int col, int[] curSum, int[] minSum) {
+        // 如果已经到达终点
+        if (row == grid.length - 1 && col == grid[0].length - 1) {
+            curSum[0] += grid[row][col];
+
+            // 更新最小的和
+            if (curSum[0] < minSum[0]) {
+                minSum[0] = curSum[0];
+            }
+
+            //curSum[0] -= grid[row][col];
+        }
+        // 还未到达终点，并且在网格内
+        else if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length) {
+            curSum[0] += grid[row][col];
+            // 当前的和只有不小于记录到的最小路径值才能进行下一步操作
+            if (curSum[0] <= minSum[0]) {
+                // 向右走
+                solve(grid, row, col + 1, curSum, minSum);
+                // 向下走
+                solve(grid, row + 1, col, curSum, minSum);
+            }
+            //curSum[0] -= grid[row][col];
+        }
+    }
 }
