7af6c727e13f99aa7e3c8a796c5d520d4af76dd4
yangjiahong <jiahong9yang@126.com>
2021-11-20 18:50:29 +0800

    0 1 2 nums sort

--- src/main/java/array/NumsSort.java
+++ src/main/java/array/NumsSort.java
@@ -20,6 +20,36 @@ public class NumsSort {
         }
     }
 
+    public void sortArray012(int[] nums){
+        int lo = 0;
+        int mid = 0;
+        int hi = nums.length - 1;
+        int temp = 0;
+
+        while (mid <= hi) {
+            switch ( nums[mid] ){
+                case 0 : {
+                    temp = nums[lo];
+                    nums[lo] = nums[mid];
+                    nums[mid] = temp;
+                    lo++;
+                    mid++;
+                    break;
+                }
+                case 1 : {
+                    mid++;
+                    break;
+                }
+                case 2 : {
+                    temp = nums[mid];
+                    nums[mid] = nums[hi];
+                    nums[hi] = temp;
+                    break;
+                }
+            }
+        }
+    }
+
     public void swap(int[] nums,int from,int to){
         int tmp = nums[from];
         nums[from] = nums[to];
