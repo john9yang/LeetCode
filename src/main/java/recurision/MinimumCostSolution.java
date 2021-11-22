0f4186ab7a836ba00bb47871f0b11b7176f98da5
yangjiahong <jiahong9yang@126.com>
2021-11-22 20:35:50 +0800

    minimus cost solution

---
+++ src/main/java/recurision/MinimumCostSolution.java
@@ -0,0 +1,45 @@
+package recurision;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MinimumCostSolution {
+    int[] costs;
+    Integer[] memo;
+    Set<Integer> dayset;
+
+    public int mincostTickets(int[] days, int[] costs) {
+        this.costs = costs;
+        memo = new Integer[366];
+        dayset = new HashSet();
+        for (int d: days) dayset.add(d);
+
+        return dp(1);
+    }
+
+    public int dp(int i) {
+        if (i > 30)
+            return 0;
+        if (memo[i] != null)
+            return memo[i];
+
+        int ans;
+        if (dayset.contains(i)) {
+            ans = Math.min(dp(i+1) + costs[0],
+                    dp(i+7) + costs[1]);
+            ans = Math.min(ans, dp(i+30) + costs[2]);
+        } else {
+            ans = dp(i+1);
+        }
+
+        memo[i] = ans;
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        MinimumCostSolution solution = new MinimumCostSolution();
+        int[] days = {1,4,6,7,8,20};
+        int[] cost = {2,7,15};
+        System.out.println(solution.mincostTickets(days,cost));
+    }
+}
